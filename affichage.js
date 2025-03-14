787ce9d9d4afe373f8cd158864d7c87ac73f3a26
odesmaraisBigBill <odesmarais@bigbill.com>
2025-03-14 10:37:39 -0400

    Affichage par JS complété !

--- affichage.js
+++ affichage.js
@@ -1,37 +1,37 @@
 const data = [
     {
-        _id: "carrousel1",
+        _id: "premierCarrousel",
         titre: "Carrousel 1",
         slides: [
             {
-                _id: "test1",
+                // _id: "test1",
                 couleur: "blue"
             },
             {
-                _id: "test2",
+                // _id: "test2",
                 couleur: "yellow"
             },
             {
-                _id: "test3",
+                // _id: "test3",
                 couleur: "pink"
             }
         ]
         // slides: [ "test1", "test2", "test3" ]
     },
     {
-        _id: "carrousel2",
+        _id: "deuxiemeCarrousel",
         titre: "Carrousel 2",
         slides: [
             {
-                _id: "test4",
+                // _id: "test4",
                 couleur: 'green'
             },
             {
-                _id: "test5",
+                // _id: "test5",
                 couleur: 'red'
             },
             {
-                _id: "test6",
+                // _id: "test6",
                 couleur: 'gold'
             }
         ]
@@ -87,11 +87,12 @@ function afficherCarrousel( carrousel ) {
 
 function creerRadios( carrousel ) {
     const radios = [];
+    let idx = 0;
     carrousel.slides.forEach(( slide ) => {
         const radio = document.createElement('input');
         radio.type = 'radio';
         radio.name = carrousel._id;
-        radio.id = slide._id;
+        radio.id = carrousel._id + idx++;
         radios.push( radio );
     });
     return radios;
@@ -100,31 +101,33 @@ function creerRadios( carrousel ) {
 function afficherSlides( carrousel ) {
     const div = document.createElement('div');
     div.classList.add('slides');
+    let idx = 0;
     carrousel.slides.forEach(( slide ) => {
-        div.appendChild( afficherSlide( slide ));
+        div.appendChild( afficherSlide( carrousel._id + idx++ ));
     })
     return div;
 }
 
-function afficherSlide( slide ) {
+function afficherSlide( id ) {
     const div = document.createElement('div');
-    div.classList.add(slide._id);
+    div.classList.add( id );
     return div;
 }
 
 function afficherFleches( carrousel ) {
     const div = document.createElement('div');
+    let idx = 0;
     div.classList.add( 'fleche' );
     carrousel.slides.forEach(( slide ) => {
-        div.appendChild( creerFleche( slide, 'prev' ));
-        div.appendChild( creerFleche( slide, 'next' ));
+        div.appendChild( creerFleche( carrousel._id + idx , 'prev' ));
+        div.appendChild( creerFleche( carrousel._id + idx++, 'next' ));
     })
     return div;
 }
 
-function creerFleche( slide, direction ) {
+function creerFleche( id, direction ) {
     const label = document.createElement( 'label' );
-    label.htmlFor = slide._id;
+    label.htmlFor = id;
     label.classList.add( direction );
     label.innerHTML = direction == 'prev' ? "&#x276E;" : "&#x276F;";
     return label;
@@ -132,24 +135,50 @@ function creerFleche( slide, direction ) {
 
 function afficherNavigation( carrousel ) {
     const div = document.createElement( 'div' );
+    let idx = 0;
+
     div.classList.add( 'navigation' );
+
     carrousel.slides.forEach(( slide ) => {
-        div.appendChild( creerLabelNavigation( slide ));
+        div.appendChild( creerLabelNavigation( carrousel._id + idx++ ));
     })
     return div; 
 }
 
-function creerLabelNavigation( slide ) {
+function creerLabelNavigation( id ) {
     const label = document.createElement( 'label' );
-    label.htmlFor = slide._id;
+    label.htmlFor = id;
     return label;
 }
 
 function ajouterStyling( carrousel ) {
     const style = document.createElement( 'style' );
     let contenu = "";
-    carrousel.slides.forEach(( slide ) => contenu += `.${slide._id} {background: ${ slide.couleur }}\n`);
-    console.log('contenu', contenu)
+    let idx = 0;
+
+    // Contenu des slides
+    carrousel.slides.forEach(( slide ) => contenu += `.${carrousel._id + idx++} {background: ${ slide.couleur }}\n`);
+
+    // Navigation
+    idx = 0;
+    carrousel.slides.forEach(( slide ) => {
+        const id = carrousel._id +  idx;
+        console.log( idx, carrousel.slides.length, idx < carrousel.slides.length - 1 )
+        contenu += `#${id}:checked ~ .navigation label[for="${id}"]${idx < carrousel.slides.length - 1 ? "," : " {\nbackground: rgba(0,0,0,.5);\n}"}\n`;
+        idx++
+    });
+
+    // Flèches
+    idx = 0;
+    carrousel.slides.forEach(( slide ) => {
+        const id = carrousel._id +  idx;
+        console.log( idx, carrousel.slides.length, idx < carrousel.slides.length - 1 )
+        contenu += `#${id}:checked ~ .fleche label[for="${
+            carrousel._id + (idx - 1 >= 0 ? idx - 1 : carrousel.slides.length - 1)
+        }"].prev,\n#${id}:checked ~ .fleche label[for="${carrousel._id + (idx + 1 < carrousel.slides.length ? idx + 1 : 0)}"].next${idx < carrousel.slides.length - 1 ? "," : " {\ndisplay: block;\n}"}\n`;
+        idx++
+    });
+    
     style.innerHTML = contenu;
     return style;
 }
